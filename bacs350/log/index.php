0f45b58916ff689b49a9610b65e997f58f570f10
Robodav <dvandiver64@gmail.com>
2019-10-25 13:14:29 -0600

    Update to current

--- bacs350/log/index.php
+++ bacs350/log/index.php
@@ -9,7 +9,7 @@
     $logs = query_logs($db);
 
     // Build a list of logs in HTML
-    $content = render_log_list($logs);
+    $content = render_log($logs);
 
     echo render_page($site_title, $page_title, $content);
 
--- bacs350/log/log.php
+++ bacs350/log/log.php
@@ -4,7 +4,7 @@ function add_row($db, $text) {
     date_default_timezone_set("America/Denver");
     $date = date('Y-m-d g:i:s a');  
     // Add database row
-    $query = "INSERT INTO log (date, text) VALUES (:date, :text);";
+    $query = "INSERT INTO logs (date, text) VALUES (:date, :text);";
     $statement = $log->prepare($query);
     $statement->bindValue(':date', $date);
     $statement->bindValue(':text', $text);
@@ -20,7 +20,7 @@ function add_log($db, $text) {
         $date = date('Y-m-d g:i:s a');
 
         // Add database row
-        $query = "INSERT INTO log (date, text) VALUES (:date, :text);";
+        $query = "INSERT INTO logs (date, text) VALUES (:date, :text);";
         $statement = $log->prepare($query);
         $statement->bindValue(':date', $date);
         $statement->bindValue(':text', $text);
@@ -36,30 +36,36 @@ function add_log($db, $text) {
 
 // Delete all logs
 function delete_logs($db) {
-    $query = "DELETE FROM log";
-    $statement = $log->prepare($query);
+    $query = "DELETE FROM logs";
+    $statement = $db->prepare($query);
     $row_count = $statement->execute();
     return true;
 }
 
 // Query for all logs
 function query_logs($db) {
-    $query = "SELECT * FROM log";
-    $statement = $db->prepare($query);
-    $statement->execute();
-    return $statement->fetchAll();
+    try {
+        $query = "SELECT * FROM logs";
+        $statement = $db->prepare($query);
+        $statement->execute();
+        return $statement->fetchAll();
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        echo "<p>Error: $error_message</p>";
+        die();
+    }
+    
 }
 
 // render_log_list -- Create a bullet list in HTML
-function render_log_list($logs) {
-    $s = '<ul>';
-    foreach($logs as $row) {
-        $date = "Date: <b>$row[date]</b>";
-        $text = "$row[text]";
-        $s .= "<li>$date : $text</li>";
+function render_log($db) {
+    $list = query_logs($db);
+    $text = '<h3>Application History</h3><ul>';
+    foreach ($list as $s) {
+        $text .= '<li>' . $s['date'] . ', ' . $s['text'] . '</li>';
     }
-    $s .= '</ul>';
-    return $s;
+    $text .= '</ul>';
+    return $text;     
 }
 
    /* -------------------------------
@@ -81,7 +87,7 @@ function render_log_list($logs) {
 
     // Connect to the Bluehost database
     function bluehost_connect() {
-        $dbname = 'dvandive_log';
+        $dbname = 'dvandive_logs';
         $username = 'dvandive_robodav';
         $password = 'BACS350';
         $port = '3306';
